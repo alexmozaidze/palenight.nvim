ab9ee9beb194fc584d9ed52cfbbab968f34299be
Alexei Mozaidze <lexerpexer@proton.me>
2023-11-14 01:48:01 +0400

    chore: generated lua

--- lua/palenight/init.lua
+++ lua/palenight/init.lua
@@ -4,6 +4,12 @@
  local function warn(message) _G.assert((nil ~= message), "Missing argument message on fnl/palenight/init.fnl:4")
  return vim.notify(("palenight.nvim: " .. message), vim.log.levels.WARN) end
 
+ local function supported_nvim_version_3f() local min_version = 8
+
+ local _let_1_ = vim.version() local version = _let_1_["minor"]
+
+ return (min_version <= version) end
+
  local M = {}
 
  M.config = {italic = true}
@@ -14,9 +20,8 @@
  M.config = new_config return nil end
 
  M.load = function()
- do local unsupported_nvim_version_3f = (vim.version().minor <= 7)
- if unsupported_nvim_version_3f then
- warn(messages["unsupported-nvim-version"]) else end end
+ if not supported_nvim_version_3f() then
+ warn(messages["unsupported-nvim-version"]) else end
  if (vim.opt.background == "light") then
  warn(messages["unsupported-light-theme"]) else end
  do local colorscheme_loaded_3f = vim.g.colors_name
@@ -24,10 +29,8 @@
  vim.cmd.hi("clear") else end end vim.g.colors_name = "palenight"
 
  local groups = require("palenight/groups")
- for _, group in ipairs(groups) do
- local hl = group.hl
- group.hl = nil
- group.italic = (M.italic and group.italic)
- vim.api.nvim_set_hl(0, hl, group) end return nil end
+ for group, hl in pairs(groups) do
+ hl.italic = (M.italic and hl.italic)
+ vim.api.nvim_set_hl(0, group, hl) end return nil end
 
  return M
